404858d87e4d1914127e35c4b69bd0d86fcaa4ce
keshavOG <keshavkeshav370@gmail.com>
2025-10-07 14:52:46 +0530

    blinky updated

--- README.md
+++
@@ -1,2 +0,0 @@
-# freeRTOS-on-esp32
-academic freeRTOS projects on esp32 chipset
--- blinky_freeRTOS/src/main.cpp
+++ blinky_freeRTOS/src/main.cpp
@@ -1,18 +1,43 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+//using only one core
+#if CONFIG_FREERTOS_UNICORE
+static const BaseType_t app_cpu = 0;
+#else 
+static const BaseType_t app_cpu = 1;
+#endif
 
+//pins
+static const int Ledpin = 2;
+
+//our task to blink an led
+void toggleLED(void *paramter){
+  while (1)
+  {
+    digitalWrite(Ledpin, HIGH);
+    vTaskDelay(500/portTICK_PERIOD_MS);
+    digitalWrite(Ledpin,LOW);
+    vTaskDelay(500/portTICK_PERIOD_MS);
+  }
+  
+}
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+
+  Serial.begin(115200);
+  pinMode(Ledpin,OUTPUT);
+  //creating task
+  xTaskCreatePinnedToCore(  // use xTaskCreate in vanilla FreeRTOS
+    toggleLED, // function to be called
+    "blinkingLED",// name of the task
+    1024, // stack size 
+    NULL, // parameter to be passed
+    1, // priority
+    NULL, // task handle
+    app_cpu // core to run the task
+  );
+ 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
